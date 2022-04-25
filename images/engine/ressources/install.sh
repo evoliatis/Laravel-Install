d72850e40492db8ca5792c023e41c6586a2f4d96
Samuel COLLADO <scollado@evoliatis.com>
2022-04-25 10:23:53 +0200

    Correction du script

--- images/engine/ressources/install.sh
+++ images/engine/ressources/install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-if [ -d /var/www/html/public ] ; then
-  echo "PUBLIC directory already exist..."
-  echo "SORRY : I don't want to break your actual project"
-  exit 1
-fi
+mkdir -p /var/www/html/public
 
 echo "Install Laravel Installer"
 composer global require "laravel/installer"
